d42cd05eacf0fa8a89398fc3b0476cfef228ce74
Reynald Borer <reynald.borer@paper.li>
2014-10-31 14:28:28 +0100

    Override async timeouts, logging when post-points fails
    Make it possible to override async timeouts by using parameters ``async-get-opts``` and ```async-post-opts``` in the client.
    
    Also, the post-points-req will now log in warning level when it receive an error.

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
     :name "Eclipse Public License"
     :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure    "1.5.1"]
-    [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
-    [clj-http               "0.9.1"]
-    [http-kit               "2.1.18"]
-    [cheshire               "5.3.1"]
+    [org.clojure/clojure       "1.5.1"]
+    [org.clojure/core.async    "0.1.319.0-6b1aca-alpha"]
+    [clj-http                  "0.9.1"]
+    [http-kit                  "2.1.18"]
+    [cheshire                  "5.3.1"]
+    [org.clojure/tools.logging "0.3.1"]
   ]
   :plugins [
     [codox           "0.6.6"]
