9105a0bc57c0b43954d74b8e40c7f2e9b2d4c91b
Reynald Borer <reynald.borer@paper.li>
2014-11-17 11:10:06 +0100

    New ping command to get cluster status

--- src/capacitor/core.clj
+++ src/capacitor/core.clj
@@ -44,7 +44,9 @@
     (client :host)
     ":"
     (client :port)
-    (cond (contains? #{ :create-admin-user
+    (cond (contains? #{:ping} (action :action))
+          "/ping"
+          (contains? #{ :create-admin-user
                         :delete-admin-user
                         :update-admin-user
                         :get-admin-users } (action :action))
@@ -114,6 +116,24 @@
 
 (def gen-url
   (memoize gen-url-multi))
+
+(defn- kw-parse-string
+  "Parse the JSON string, coerce keys to keywords"
+  [string]
+  (json/parse-string string true))
+
+(defn ping
+  [client]
+  (-> client
+      (gen-url :ping)
+      (http-client/get 
+        (merge {:socket-timeout       1000
+                :conn-timeout         1000
+                :accept               :json
+                :trow-entire-message? true}
+               (:get-opts client)))
+      :body
+      kw-parse-string))
 
 ;;
 ;; ## Database management
